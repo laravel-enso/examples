f081af30d6e37fd22162b0fdeada848f0b9170a1
mishu <mishu@xtelecom.ro>
2018-09-16 17:58:43 +0300

    adds factory to publishable

--- src/AppServiceProvider.php
+++ src/AppServiceProvider.php
@@ -24,6 +24,15 @@ class AppServiceProvider extends ServiceProvider
         $this->loadRoutesFrom(__DIR__.'/routes/web.php');
         $this->loadMigrationsFrom(__DIR__.'/database/migrations');
         $this->loadViewsFrom(__DIR__.'/resources/views', 'laravel-enso/examples');
+
+        $this->publishes([
+            __DIR__.'/database/factories' => database_path('factories'),
+        ], 'examples-factory');
+
+        $this->publishes([
+            __DIR__.'/database/factories' => database_path('factories'),
+        ], 'enso-factories');
+
     }
 
     public function register()
